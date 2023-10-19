0276b7072961f1266c5000f6cb5aa39f193e8989
imSmaity <sumanmaity305@gmail.com>
2023-10-20 00:59:11 +0530

    add all

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="maaa.png" alt="..." style={{ width: '10%', height: '10%' }} />
+        <h1>শারদীয়া দুর্গাপূজা ২০২৩</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          অ্যাপটির সাহায্যে আপনাদের পছন্দের পুজো প্যান্ডেল গুলি খুঁজে পেতে পারেন
+          <p>অ্যাপটি ডাউনলোড করতে "Download App" এ ক্লিক করুন</p>
         </p>
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="puja_pandal.apk"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <button style={{ width: '200px', padding: '15px', fontSize: '22px' }}>
+            Download App
+          </button>
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
